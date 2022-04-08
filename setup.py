8c5ccc5fbc81cfaa9b9a7e6078265af953e322a4
vlaurent <vincent.laurent.dsw@gmail.com>
2022-04-08 10:42:23 +0200

    Post install step

--- setup.py
+++ setup.py
@@ -1,4 +1,46 @@
+from distutils.command.install import install
+
 from setuptools import setup
+
+
+class PostInstall(install):
+    def run(self):
+        import sys
+        import rpy2.robjects as ro
+        r_session = ro.r
+        install.run(self)
+        package_list = ["survival", "LTRCtrees", "data.table",
+         "rpart", "Icens", "interval", 'stringi', "hash"]
+        if len(package_list) == 1:
+            package_list = f"('{package_list[0]}')"
+        else:
+            package_list = str(tuple(
+                package_list
+            ))
+        r_cmd = """
+                   packages = c""" + package_list + """
+                   package.check <- lapply(
+                     packages,
+                     FUN = function(x) {
+                       if (!require(x, character.only = TRUE)) {
+                         install.packages(x, dependencies = TRUE)
+                         library(x, character.only = TRUE)
+                       }
+                     }
+                   )"""
+        r_session(r_cmd)
+
+        if "linux" in sys.platform:
+            r_cmd2 = """if (!require("LTRCtrees", character.only = TRUE)) {
+                      install.packages("https://cran.r-project.org/src/contrib/Archive/LTRCtrees/LTRCtrees_1.1.0.tar.gz")
+                    }"""
+        else:
+            r_cmd2 = """if (!require("LTRCtrees", character.only = TRUE)) {
+                      install.packages("https://cran.microsoft.com/snapshot/2017-08-01/bin/windows/contrib/3.4/LTRCtrees_0.5.0.zip")
+                    }"""
+
+        r_session(r_cmd2)
+        # raise AssertionError("Passed")
 
 
 def parse_requirements(filename):
@@ -23,4 +65,5 @@ setup(name='survival_trees',
                    },
       include_package_data=True,
       package_data={'survival_trees': ['*.R']},
+      cmdclass=dict(install=PostInstall),
       zip_safe=False)
