72358d3f1839070f91054069ad533cd1967df47a
vlaurent <vlaurent@eurobios.com>
2022-04-08 15:57:58 +0200

    Setup with sources

--- setup.py
+++ setup.py
@@ -2,6 +2,18 @@ from distutils.command.install import install
 
 from setuptools import setup
 
+r_packages = {
+    "LTRCtrees": "https://cran.r-project.org/src/contrib/Archive/LTRCtrees/LTRCtrees_1.1.0.tar.gz",
+    "icens": "https://cran.r-project.org/src/contrib/Archive/Icens/Icens_1.24.0.tar.gz",
+    "interval": "https://cran.r-project.org/src/contrib/interval_1.1-0.8.tar.gz"
+    }
+
+r_packages_win = {
+    "LTRCtrees": "https://cran.microsoft.com/snapshot/2017-08-01/bin/windows/contrib/3.4/LTRCtrees_0.5.0.zip",
+    "icens": "https://cran.microsoft.com/snapshot/2017-08-01/bin/windows/contrib/3.4/Icens_1.24.0.tar.gz",
+    "interval": "https://cran.r-project.org/bin/windows/contrib/4.2/interval_1.1-0.8.zip"
+}
+
 
 class PostInstall(install):
     def run(self):
@@ -9,8 +21,8 @@ class PostInstall(install):
         import rpy2.robjects as ro
         r_session = ro.r
         install.run(self)
-        package_list = ["survival", "LTRCtrees", "data.table",
-         "rpart", "Icens", "interval", 'stringi', "hash"]
+        package_list = ["survival", "data.table",
+         "rpart", 'stringi', "hash"]
         if len(package_list) == 1:
             package_list = f"('{package_list[0]}')"
         else:
@@ -31,15 +43,14 @@ class PostInstall(install):
         r_session(r_cmd)
 
         if "linux" in sys.platform:
-            r_cmd2 = """if (!require("LTRCtrees", character.only = TRUE)) {
-                      install.packages("https://cran.r-project.org/src/contrib/Archive/LTRCtrees/LTRCtrees_1.1.0.tar.gz")
-                    }"""
+            packages_to_compile = r_packages
         else:
-            r_cmd2 = """if (!require("LTRCtrees", character.only = TRUE)) {
-                      install.packages("https://cran.microsoft.com/snapshot/2017-08-01/bin/windows/contrib/3.4/LTRCtrees_0.5.0.zip")
+            packages_to_compile = r_packages_win
+        for k, v in packages_to_compile.items():
+            r_cmd2 = """if (!require('""" + k + """', character.only = TRUE)) {
+                      install.packages('""" + v + """')
                     }"""
-
-        r_session(r_cmd2)
+            r_session(r_cmd2)
         # raise AssertionError("Passed")
 
 
